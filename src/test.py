3005da9aafe3e018949bd4233c99e31460ce48fd
Faustyx <fautyx.77@gmail.com>
2024-11-26 14:46:09 +0100

    Premier test avec la br dev

--- src/test.py
+++ src/test.py
@@ -1,2 +1,5 @@
-def firstTest():
-	return("<h1>TESTOUI</h1>")
+def firstTest(value):
+	if value == "Nathan":
+        	return("<h1>Test Réussi, valeur : "+value+"</h1>")
+	else:
+		return("<h1>Test Échoué</h1>")

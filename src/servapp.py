facf0725a82eeffcbb661c5c07a6825b0f5b37cc
Faustyx <fautyx.77@gmail.com>
2024-11-27 14:27:30 +0100

    rapide test

--- src/servapp.py
+++ src/servapp.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return firstTest("Nathan")
+	print("test")
+	return True
+    #return firstTest("Nathan")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')

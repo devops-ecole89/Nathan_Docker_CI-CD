e3240f132863ef3603dd1b435781ca2e39798d1d
Faustyx <fautyx.77@gmail.com>
2024-11-27 14:43:29 +0100

    oui

--- src/servapp.py
+++ src/servapp.py
@@ -1,13 +1,16 @@
+import sys
 from flask import Flask
 from test import firstTest
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-	print("test")
-	return True
+	print (sys.argv)
+#	print("test")
+	return "<h1>hello<h1>"
     #return firstTest("Nathan")
 
 if __name__ == "__main__":
-	print("main function")
-    app.run(host='0.0.0.0')
+	hello()
+#    app.run(host='0.0.0.0')
